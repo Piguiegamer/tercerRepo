150a14962d8416b0f7d24a3d88faa647b8e15346
Piguiegamer <ae.joseuibarrah@gmail.com>
2026-06-23 22:52:10 -0600

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Piguiegamer",                         # Tu nombre
+    author_email="piguiegamer@proton.me",                 # Tu correo electrónico
+    url="https://github.com/piguiegamer/tercerRepo",     # URL del proyecto
+)
